8fc9979c93eb579314cab34c771fbf7c54ae3d12
bgymn <105134287+bgymn@users.noreply.github.com>
2022-07-02 00:44:36 -0600

    Delete docs/assets/showcase directory

--- docs/assets/showcase/main.js
+++
@@ -1,9 +0,0 @@
-/*global document*/
-
-// Modal Image Gallery
-function onClick(element) {
-  document.getElementById("img01").src = element.src;
-  document.getElementById("modal01").style.display = "block";
-  var captionText = document.getElementById("caption");
-  captionText.innerHTML = element.alt;
-}
