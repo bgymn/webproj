946118d313ff2c72b8dacc6a768cef93f1b15c5e
bgymn <105134287+bgymn@users.noreply.github.com>
2022-07-01 22:32:47 -0600

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,14 @@ function myFunction() {
     }
 }
 
+// Modal Image Gallery
+function onClick(element) {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+  var captionText = document.getElementById("caption");
+  captionText.innerHTML = element.alt;
+}
+
 // Used to toggle the menu on small screens when clicking on the menu button
 function toggleFunction() {
     var x = document.getElementById("navDemo");
